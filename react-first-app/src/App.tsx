e87f304bb9ba138d51a012e42e38f62b5c7d8337
Om Prakash Sahoo <omprakashsahoo1234@gmail.com>
2024-06-04 20:46:55 +0530

    Update: Necessary comments added to help understand the code better.

--- react-first-app/src/App.tsx
+++ react-first-app/src/App.tsx
@@ -23,8 +23,11 @@ function App() {
       <ListGroup items={items} heading="Most Loved Countries" onSelectItem={handleonSelectItem} />
       {/* <Alert>Sorry that was it from my side</Alert> */}
 
-
+      {/* when the first condition is true second condition returns  */}
       {alertVisible && <Alert onClick={() => SetAlertVisible(false)}>You clicked and I got your IP address</Alert>}
+
+      {/* The below thing is called lifting state up in react, since we are using app tsx functions and strings 
+      to change the contents in below components which is lower in virtual dom using props */}
       <ButtonClassic color="bg-purple-500" onClick={() => SetAlertVisible(true)}>Click Me!</ButtonClassic>
     </div>
   );
